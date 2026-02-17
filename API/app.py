695b7c0bed687fbbabb7a16557029bbaac45d9f1
Kevin <kevinrivadeneira724@gmail.com>
2026-02-17 16:02:58 -0500

    Añadí los cursos de Platzi

--- API/app.py
+++ API/app.py
@@ -34,3 +34,8 @@ def get_starwars_warriors():
 def get_warriors_got():
     rows = ["Jon Snow", "Daenerys Targaryen", "Arya Stark", "Tyrion Lannister", "Cersei Lannister", "Jaime Lannister", "Bran Stark", "Sansa Stark"]
     return rows
+
+@app.get("/cursosPlatzi")
+def get_cursos():
+    rows= ["Docker", "Bash", "Linux", "Inglés", "Python", "JavaScript", "Azure"]
+    return rows
